287f2cd7965a5a880a528443328e782318d2b1b9
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-02-17 09:36:32 -0500

    camelCase passed

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -9,7 +9,9 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+       String firstLetter = str.substring(0,1).toUpperCase();
+       String restOfString = str.substring(1);
+       return firstLetter+restOfString;
     }
 
     /**
