733b39ac51ecafbd2e899c09d8cc0f732fe8048c
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-02-17 15:10:05 -0500

    passes one part 5 test

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -2,7 +2,31 @@ package com.zipcodewilmington.assessment1.part5;
 
 public class Palindrome {
 
-    public Integer countPalindromes(String input){
-        return null;
+    public Integer countPalindromes(String input) {
+        int counter = 0;
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = 0; j < input.length(); j++) {
+                if (j>=i){
+                    if (input.substring(i, j).equals(reverse(input.substring(i, j)))){
+                        counter++;
+                    }
+
+                 }
+                }
+            }
+        return counter;
+        }
+
+
+
+
+
+
+    public static String reverse(String str) {
+        String reversed = "";
+        for(int p = str.length()-1;p>=0;p--){
+            reversed+= str.charAt(p);
+        }
+        return reversed;
     }
 }
